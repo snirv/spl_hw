8c8b8313833a191380d0335b1549cd23fe971c03
Snir Vaknin <snir.vaknin@gmail.com>
2018-01-05 22:56:08 +0200

    sharon msg

--- src/main/java/bgu/spl181/net/api/bidi/Movie.java
+++ src/main/java/bgu/spl181/net/api/bidi/Movie.java
@@ -3,6 +3,7 @@ package bgu.spl181.net.api.bidi;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
+//I am shron
 public class Movie {
     protected Integer id;
     protected String name;
